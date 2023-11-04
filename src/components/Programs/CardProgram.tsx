ee65c42dd41b241f1ccfdf18653ac42fa334e467
SBellido <sebastianbellidodg@gmail.com>
2023-11-04 23:58:51 +0100

    Using MUI modal without styles

--- src/components/Programs/CardProgram.tsx
+++ src/components/Programs/CardProgram.tsx
@@ -1,34 +1,61 @@
-import { useState } from 'react';
-import { CardProgramProps } from '../../interfaces/interfaces';
+import React, { useEffect, useState } from 'react';
+import { Modal, Backdrop, Fade, Button } from '@mui/material';
+import { Program } from '../../interfaces/interfaces';
+import { FilteredPrograms } from './FilteredPrograms';
 
-function CardProgram(props: CardProgramProps) {
-  const [modalOpen, setModalOpen] = useState(false);
+const Programs: React.FC<{ programType: string }> = ({ programType }) => {
+  const [programsData, setProgramsData] = useState<Program[]>([]);
+  const [selectedProgram, setSelectedProgram] = useState<Program | null>(null);
 
-  const closeModal = () => {
-    setModalOpen(false);
+  useEffect(() => {
+    const filteredData = FilteredPrograms(programType);
+    setProgramsData(filteredData);
+  }, [programType]);
+
+  const handleOpenModal = (program: Program) => {
+    setSelectedProgram(program);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedProgram(null);
   };
 
   return (
-    <div className="Card">
-      <img src={props.image.url} alt={props.title} />
-      <h2>{props.title}</h2>
-      <p>{props.description}</p>
-      <p>{props.releaseYear}</p>
-
-      {modalOpen && (
-        <div className="Modal">
-          <div className="Modal-container">
-            <span className="Close-modal" onClick={closeModal}>
-              X
-            </span>
-            <h3>{props.title}</h3>
-            <p>{props.description}</p>
-            <p>{props.releaseYear}</p>
+    <div className="Grid-container">
+      {programsData.map((item) => (
+        <div className="Grid-item" key={item.title} onClick={() => handleOpenModal(item)}>
+          <img className="Image-product" alt={item.title} src={item.images['Poster Art'].url} />
+          <div className="Containar-title--product">
+            <p>
+              <span>{item.title}</span>
+            </p>
           </div>
         </div>
-      )}
+      ))}
+
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={selectedProgram !== null}
+        onClose={handleCloseModal}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={selectedProgram !== null}>
+          <div className="modal">
+            <img className="Image-product" alt={selectedProgram?.title} src={selectedProgram?.images['Poster Art'].url} />
+            <h2 id="transition-modal-title">{selectedProgram?.title}</h2>
+            <p id="transition-modal-description">{selectedProgram?.releaseYear}</p>
+            <p id="transition-modal-description">{selectedProgram?.description}</p>
+            <Button onClick={handleCloseModal}>Close</Button>
+          </div>
+        </Fade>
+      </Modal>
     </div>
   );
-}
+};
 
-export default CardProgram;
+export { Programs };
--- src/components/Programs/Programs.tsx
+++ src/components/Programs/Programs.tsx
@@ -1,30 +1,62 @@
 import React, { useState, useEffect } from 'react';
-import { FilteredPrograms } from './ProgramFunctions';
+import { Modal, Backdrop, Fade, Button } from '@mui/material';
+import { FilteredPrograms } from './FilteredPrograms';
 import { Program } from '../../interfaces/interfaces';
 import '../../slyles/Programs.css';
 
 const Programs: React.FC<{ programType: string }> = ({ programType }) => {
   const [programsData, setProgramsData] = useState<Program[]>([]);
+  const [selectedProgram, setSelectedProgram] = useState<Program | null>(null);
 
   useEffect(() => {
     const filteredData = FilteredPrograms(programType);
     setProgramsData(filteredData);
   }, [programType]);
 
+  const handleOpenModal = (program: Program) => {
+    setSelectedProgram(program);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedProgram(null);
+  };
+
   return (
     <div className="Grid-container">
       {programsData.map((item) => (
-        <div className='Grid-item'>
-          <img className='Image-product' alt={item.title} 
-            src={item.images['Poster Art'].url} />
-            <div className='Containar-title--product'> 
-             <p><span>{item.title}</span></p>
-            </div>
+        <div className="Grid-item" key={item.title} onClick={() => handleOpenModal(item)}>
+          <img className="Image-product" alt={item.title} src={item.images['Poster Art'].url} />
+          <div className="Containar-title--product">
+            <p>
+              <span>{item.title}</span>
+            </p>
+          </div>
         </div>
       ))}
+
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={selectedProgram !== null}
+        onClose={handleCloseModal}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={selectedProgram !== null}>
+          <div className="modal">
+            <img className="Image-product" alt={selectedProgram?.title} src={selectedProgram?.images['Poster Art'].url} />
+            <h2 id="transition-modal-title">{selectedProgram?.title}</h2>
+            <p id="transition-modal-description">{selectedProgram?.releaseYear}</p>
+            <p id="transition-modal-description">{selectedProgram?.description}</p>
+            <Button onClick={handleCloseModal}>Close</Button>
+          </div>
+        </Fade>
+      </Modal>
     </div>
   );
 };
 
 export { Programs };
-
